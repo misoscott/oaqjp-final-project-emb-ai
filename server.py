33062ec80a2a9bab568e8dd34693ddafbe2d5dd8
misoscott <scott@hyperthought.com>
2026-02-15 19:23:59 -0500

    add error handling, pylint fixes

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-''' this is the module docstring '''
+''' 
+ this is the server module for the emotion detector project 
+'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -10,20 +12,20 @@ def emotion_detect():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     return_str = "For the given statement, the system response is "
-    for item in response:
+    for item, val in response.items():
         if item == "dominant_emotion":
-            return_str += ". The dominant emotion is " + str(response[item]) + "."
+            if val is None:
+                return "Invalid text! Please try again!"
+            return_str += ". The dominant emotion is " + str(val) + "."
             break
-        return_str += "'" + item + "':" + str(response[item]) + ", "
+        return_str += ", " + "'" + item + "':" + str(val)
 
     return return_str
 
 @app.route('/')
 def render_index_page():
-    ''' this is the docstring for render_index_page'''
+    ''' this is the docstring for render_index_page '''
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
